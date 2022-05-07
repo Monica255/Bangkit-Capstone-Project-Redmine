9deffbb8a810eccdb70261ce2d466781bea241e3
Monica <monicasucianto123@gmail.com>
2022-05-07 19:43:34 +0800

    Capstone project redmine

---
+++ app/src/test/java/com/example/redminecapstoneproject/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.redminecapstoneproject
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
