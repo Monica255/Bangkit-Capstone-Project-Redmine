d1a434b8cfe7756744e7b6a4e879f982460aa045
Monica <monicasucianto123@gmail.com>
2022-05-11 23:46:21 +0800

    progress 11 Mei continue working on blood donors and make donor request -create donor request -blood donor detail

---
+++ app/src/main/java/com/example/redminecapstoneproject/CreateDonorReqActivity.kt
@@ -0,0 +1,11 @@
+package com.example.redminecapstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateDonorReqActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_donor_req)
+    }
+}
